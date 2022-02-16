587383f3aae2c6baf4f441a9554cc161bc003452
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-16 19:36:25 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,15 @@
+const insult = (name) => {
+  console.log(`${name} idiot`);
+}
+insult("Fandy Adianto");
+
+function greet(name) {
+  console.log(`Halo ${name}`)
+}
+console.log(greet());
+
+function lol() {
+  console.log("lollol");
+  return "k";
+}
+console.log(lol())
